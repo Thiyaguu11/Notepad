925c11b62490cdd997468721af53404fde36c401
Thiyaguu11 <151392384+Thiyaguu11@users.noreply.github.com>
2023-12-16 13:06:30 +0530

    base code added

---
+++ base_code/notepad.py
@@ -0,0 +1,91 @@
+import tkinter as tk
+from tkinter import filedialog, messagebox
+
+class Notepad:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Notepad")
+        self.root.geometry("600x400")
+
+        self.text_widget = tk.Text(self.root, wrap="word", undo=True)
+        self.text_widget.pack(expand=True, fill="both")
+
+        self.menu_bar = tk.Menu(self.root)
+        self.root.config(menu=self.menu_bar)
+
+        # File menu
+        file_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="File", menu=file_menu)
+        file_menu.add_command(label="New", command=self.new_file)
+        file_menu.add_command(label="Open", command=self.open_file)
+        file_menu.add_command(label="Save", command=self.save_file)
+        file_menu.add_command(label="Save As", command=self.save_as_file)
+        file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.root.destroy)
+
+        # Edit menu
+        edit_menu = tk.Menu(self.menu_bar, tearoff=0)
+        self.menu_bar.add_cascade(label="Edit", menu=edit_menu)
+        edit_menu.add_command(label="Undo", command=self.undo)
+        edit_menu.add_command(label="Redo", command=self.redo)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Cut", command=self.cut)
+        edit_menu.add_command(label="Copy", command=self.copy)
+        edit_menu.add_command(label="Paste", command=self.paste)
+        edit_menu.add_separator()
+        edit_menu.add_command(label="Select All", command=self.select_all)
+
+    def new_file(self):
+        self.text_widget.delete(1.0, tk.END)
+
+    def open_file(self):
+        file_path = filedialog.askopenfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+        if file_path:
+            with open(file_path, "r") as file:
+                content = file.read()
+                self.text_widget.delete(1.0, tk.END)
+                self.text_widget.insert(tk.END, content)
+            self.root.title(f"Notepad - {file_path}")
+
+    def save_file(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
+        if file_path:
+            with open(file_path, "w") as file:
+                content = self.text_widget.get(1.0, tk.END)
+                file.write(content)
+            self.root.title(f"Notepad - {file_path}")
+
+    def save_as_file(self):
+        self.save_file()
+
+    def undo(self):
+        try:
+            self.text_widget.edit_undo()
+        except tk.TclError:
+            pass
+
+    def redo(self):
+        try:
+            self.text_widget.edit_redo()
+        except tk.TclError:
+            pass
+
+    def cut(self):
+        self.text_widget.event_generate("<<Cut>>")
+
+    def copy(self):
+        self.text_widget.event_generate("<<Copy>>")
+
+    def paste(self):
+        self.text_widget.event_generate("<<Paste>>")
+
+    def select_all(self):
+        self.text_widget.tag_add(tk.SEL, "1.0", tk.END)
+        self.text_widget.mark_set(tk.SEL_FIRST, "1.0")
+        self.text_widget.mark_set(tk.SEL_LAST, tk.END)
+        self.text_widget.see(tk.SEL_FIRST)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    notepad = Notepad(root)
+    root.mainloop()
